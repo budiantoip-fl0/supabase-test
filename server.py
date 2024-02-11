4ba717abcf6e850a2a872dfe7419087f9edf7427
Budianto IP <budiantoip@gmail.com>
2024-02-11 11:54:58 +0700

    add templates

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 import os
 from supabase import create_client, Client
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def hello():
