b9c149001a8889dca1f50e43bdeab4278f99c1ca
Budianto IP <budiantoip@gmail.com>
2024-02-11 11:49:32 +0700

    add html

--- server.py
+++ server.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+
+import os
+from supabase import create_client, Client
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+
+    url: str = os.environ.get("SUPABASE_URL")
+    key: str = os.environ.get("SUPABASE_KEY")
+    supabase: Client = create_client(url, key)
+
+    response = supabase.table('Test').select("*").execute()
+
+    print(response)
+
+    return render_template('index.html', data=response)
